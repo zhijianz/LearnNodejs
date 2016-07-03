060b23ab862ea3140681a5e15f97a100f3bbd96c
zhijianz <zhijianz@yeah.net>
2016-07-03 17:27:10 +0800

    add example 3 project

---
+++ Node.js_Teach_Yourself/Examples/example_03/io.js
@@ -0,0 +1,14 @@
+var http = require('http');
+var urls = ['www.baidu.com', 'www.bilibili.com', 'www.google.com'];
+
+function fetchResponse(url) {
+  var start = new Date();
+  http.get({host:url}, function(res) {
+    console.log('Got response from: ' + url);
+    console.log('Request took: ', new Date() - start, 'ms');
+  });
+}
+
+for (var i = 0; i < urls.length; i++) {
+    fetchResponse(urls[i]);
+  }
